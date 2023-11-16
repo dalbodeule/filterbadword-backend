fdccd011c9bcbc7966c63e2131217f97878096fc
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2023-11-16 21:45:30 +0900

    update okt

--- route/isbadword.py
+++ route/isbadword.py
@@ -2,14 +2,14 @@ from fastapi import HTTPException, Security, APIRouter
 import tensorflow as tf
 import fasttext
 from auth import get_api_key
-from konlp.kma.klt2023 import klt2023
+from konlpy.tag import Okt
 
 from os import path
 import os
 
 TFModel = tf.keras.models.load_model(path.join(os.getcwd(), 'saved_model.h5'))
 fastTextModel = fasttext.load_model(path.join(os.getcwd(), 'fasttext.bin'))
-klt = klt2023()
+okt = Okt()
 
 def is_float(element: any) -> bool:
     #If you expect None to be passed:
@@ -26,7 +26,7 @@ ncf = APIRouter(prefix='/api')
 @ncf.post('/')
 async def predict(text: str, api_key: str = Security(get_api_key)):
     try:
-        data = klt.morphs(str(text).strip())
+        data = okt.morphs(str(text).strip())
         p = []
         for q in data:
             if (not is_float(q)) and q != '_':
