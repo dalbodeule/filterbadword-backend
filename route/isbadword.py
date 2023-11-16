339d6372b56015296a78a8e89222c382ea6ff407
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2023-11-16 21:29:39 +0900

    add konlp

--- route/isbadword.py
+++ route/isbadword.py
@@ -2,23 +2,38 @@ from fastapi import HTTPException, Security, APIRouter
 import tensorflow as tf
 import fasttext
 from auth import get_api_key
+from konlp.kma.klt2023 import klt2023
 
 from os import path
 import os
 
 TFModel = tf.keras.models.load_model(path.join(os.getcwd(), 'saved_model.h5'))
 fastTextModel = fasttext.load_model(path.join(os.getcwd(), 'fasttext.bin'))
+klt = klt2023()
+
+def is_float(element: any) -> bool:
+    #If you expect None to be passed:
+    if element is None:
+        return False
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
 
 ncf = APIRouter(prefix='/api')
 
 @ncf.post('/')
 async def predict(text: str, api_key: str = Security(get_api_key)):
     try:
-        data = str(text).strip().split()
-        if len(data) == 0:
+        data = klt.morphs(str(text).strip())
+        p = []
+        for q in data:
+            if (not is_float(q)) and q != '_':
+                p.append(q)
+        if len(p) == 0:
             return { "predition": 0.0 }
-
-        data = [fastTextModel[i] for i in data]
+        data = [fastTextModel[i] for i in p]
 
         data = tf.ragged.constant([data])
 
